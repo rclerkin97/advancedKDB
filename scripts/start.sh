2d8997c5958ab108d152e7561cbc540e73a4e640
rclerkin97 <131470355+rclerkin97@users.noreply.github.com>
2023-05-10 23:28:44 +0100

    Add files via upload

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,7 +12,7 @@ echo "Enter y to run the Tickerplant, (y/n)"
 read tickbool
 
 if [ "$tickbool" = "y" ]; then
-        nohup $q $advancedKDB/tickerplant.q sym . -p $tpPort 1>>$advancedKDB/logs/tickerplant.log 2>&1 &
+        q $advancedKDB/tickerplant.q sym . -p $tpPort 1>>$advancedKDB/logs/tickerplant.log 2>&1 &
 fi
 
 #Start trade and qoute RDBs
@@ -20,7 +20,7 @@ echo "Enter y to run the Trade/Quote RDB (y/n)?"
 read rTQbool
 
 if [ "$rTQbool" = "y" ]; then
-    nohup $q $advancedKDB/tick/rdbTQ.q localhost:$tpPort 1>>$advancedKDB/logs/rdbTQ.log 2>&1 &
+        q $advancedKDB/tick/rdbTQ.q localhost:$tpPort 1>>$advancedKDB/logs/rdbTQ.log 2>&1 &
 fi
 
 #Start Aggregation RDB
@@ -28,7 +28,7 @@ echo "Enter y to run the Aggregation RDB (y/n)?"
 read rAGGbool
 
 if [ "$rAGGbool" = "y" ]; then
-        nohup $q $advancedKDB/tick/rdbAGG.q localhost:$tpPort 1>>$advancedKDB/logs/rdbAGG.log 2>&1 &
+        q $advancedKDB/tick/rdbAGG.q localhost:$tpPort 1>>$advancedKDB/logs/rdbAGG.log 2>&1 &
 fi
 
 #Start Complex Event Processer/Calculation Engine
@@ -36,7 +36,7 @@ echo "Enter y to run the Start CEP (y/n)?"
 read cepbool
 
 if [ "$cepbool" = "y" ]; then
-    nohup $q $advancedKDB/tick/cep.q localhost:$tpPort 1>>$advancedKDB/logs/cep.log 2>&1 &
+        q $advancedKDB/tick/cep.q localhost:$tpPort 1>>$advancedKDB/logs/cep.log 2>&1 &
 fi
 
 #Start Feedhandler
@@ -44,5 +44,5 @@ echo "Enter y to run the Mock Feedhandler (y/n)?"
 read feedbool
 
 if [ "$feedbool" = "y" ]; then
-        nohup $q $advancedKDB/tick/feedHandler.q 1>>$advancedKDB/logs/feedhandler.log 2>&1 &
+        q $advancedKDB/tick/feedHandler.q 1>>$advancedKDB/logs/feedhandler.log 2>&1 &
 fi
